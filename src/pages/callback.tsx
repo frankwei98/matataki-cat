e8ff69670c0573a895908104906ae34a54224d9d
Frank Wei <frank@frankwei.xyz>
2021-09-01 17:00:40 +0800

    go callback

--- src/pages/callback.tsx
+++ src/pages/callback.tsx
@@ -1,11 +1,19 @@
-import { useMemo } from "react"
+import { useEffect, useMemo } from "react"
+import { useRecoilState } from "recoil";
+import { accessTokenAtom } from "../atoms/AccessToken.atom";
 import { useQuery } from "../hooks/useQuery"
 
 export function CallBackHandler() {
-    const query = useQuery()
+    const query = useQuery();
+    const [_, tokenSetter] = useRecoilState(accessTokenAtom)
     const oauthToken = useMemo(() => {
         return query.get('token')
-    }, [query])
+    }, [query]);
+
+    useEffect(() => {
+        if (!oauthToken) return;
+        tokenSetter(oauthToken);
+    }, [tokenSetter, oauthToken])
 
     if (!oauthToken) {
         return <div className="error">
